607aa259184164b9070f53d3a25ad7eaf98335bf
Eliot Carney-Seim <ecsit39@gmail.com>
2016-11-28 10:32:58 -0500

    Finishing game . . . adding character details pt 2

---
+++ Assets/Scripts/LoadDay1.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadDay1 : MonoBehaviour {
+
+    private int dayNum = -1;
+
+    public GameControls game;
+
+	// Use this for initialization
+	void Awake () {
+        dayNum++;
+        game.SpawnNPC();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
